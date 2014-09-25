e65712551726fb629d9b03077685428031a11820
bdgrider <bdgrider17@ufl.edu>
2014-09-25 17:44:51 -0400

    Dirty workaround to handle case-sensitive paths on github server (script.js)

--- assets/scripts.js
+++ assets/scripts.js
@@ -3,8 +3,14 @@
 $(document).ready(function() {
 
 	var randomPhotoNumber = (Math.round(Math.random()*5) + 1);
-	var newPhoto = randomPhotoNumber + ".jpg";
-	//var path2= 'url("/assets/1.jpg")';
+	
+	if(randomPhotoNumber==1 || randomPhotoNumber==2 || randomPhotoNumber==5) {
+		var newPhoto = randomPhotoNumber + ".JPG";
+	}
+	else {
+		var newPhoto = randomPhotoNumber + ".jpg";
+	}
+	
 	var path2 = $('#siteBanner').css('background-image');
 	path=path2.replace("1.jpg", newPhoto);
 
